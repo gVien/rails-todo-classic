a99da8cfa0af2364179ebff24604bff09df012ed
Gai Vien & Tom Lee <pair+gvien.user512@devbootcamp.com>
2015-08-20 13:30:03 -0700

    add edit feature

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :tasks
+  resources :tasks, except: [:show]
   # Example resource route with options:
   #   resources :products do
   #     member do
