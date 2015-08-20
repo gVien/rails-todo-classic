861fb783c44e319036f5a03d622e4b2c279ff84d
Gai Vien & Tom Lee <pair+gvien.user512@devbootcamp.com>
2015-08-20 15:53:06 -0700

    add all destroy, update, and complete features

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order("id ASC")
   end
 
   def new
@@ -18,6 +18,37 @@ class TasksController < ApplicationController
       redirect_to tasks_path
     else
       render "new"
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    @task.assign_attributes(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  def complete
+    task = Task.find(params[:id])
+    if task.complete == false
+      task.complete = true
+      task.save
+      redirect_to tasks_path
+    else
+      task.complete = false
+      task.save
+      redirect_to tasks_path
     end
   end
 
